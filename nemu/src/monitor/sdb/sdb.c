1034a3f73d891d15f187d21c5838326b122bff20
tracer-ysyx <tracer@ysyx.org>
2024-08-08 12:31:03 +0800

    > compile NEMU ysyx_? 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:31:03 up  4:57,  0 users,  load average: 0.25, 0.17, 0.11

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -121,7 +121,7 @@ static int cmd_x(char *args) {
   return 0;
 
 error:
-  printf("Usage: 'x N ADDR', N refers to scan length, ADDR refers to the start address, which can be a expression.\n");
+  printf("Usage: 'x N ADDR', N(int) refers to scan length, ADDR refers to the start address, which can be a expression.\n");
   return 0;
 }
 
@@ -136,10 +136,10 @@ static struct {
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
   { "si", "Single-step execution,\n\
-          \tUsage: 'si [N]', N refers to execution times, with a default value 1.", cmd_si },
+          \tUsage: 'si [N]', N(int) refers to execution times, with a default value 1.", cmd_si },
   { "info", "Display information about regs('info r') or wathcpoints('info w')", cmd_info },
   { "x", "Display memory content,\n\
-          \tUasge: 'x N ADDR', N refers to scan length, ADDR refers to the start address, which can be a expression.", cmd_x },
+          \tUasge: 'x N ADDR', N(int) refers to scan length, ADDR refers to the start address, which can be a expression.", cmd_x },
   /* TODO: Add more commands */
 
 };
