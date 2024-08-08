c7fd960a0e7a64635b3923ba70710ca43ca71f96
tracer-ysyx <tracer@ysyx.org>
2024-08-08 08:36:34 +0800

    > compile NEMU ysyx_? 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:36:34 up  1:03,  0 users,  load average: 0.22, 0.14, 0.13

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -74,7 +74,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "Single-step execution", cmd_si },
+  { "si", "Single-step execution,\n\tUsage: 'si [N]', N refers to execution times, with a default value 1", cmd_si },
   /* TODO: Add more commands */
 
 };
