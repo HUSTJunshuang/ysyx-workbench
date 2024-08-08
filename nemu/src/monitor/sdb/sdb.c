7c26316f54d1c657d613d8500008d0540c47ed02
tracer-ysyx <tracer@ysyx.org>
2024-08-08 11:39:41 +0800

    > compile NEMU ysyx_? 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:39:41 up  4:06,  0 users,  load average: 0.16, 0.15, 0.10

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -91,7 +91,6 @@ error:
 static int cmd_x(char *args) {
   paddr_t addr;
   int len;
-  char *extra = NULL;
   // process len
   char *arg = strtok(NULL, " ");
   if (arg == NULL)  goto error;
@@ -99,7 +98,7 @@ static int cmd_x(char *args) {
   // process address
   arg = strtok(NULL, " ");
   if (arg == NULL)  goto error;
-  int ret = sscanf(arg, "0x%x%s", &addr, extra);
+  int ret = sscanf(arg, "0x%x", &addr);
   printf("ret = %d\n", ret);
   if (ret == 0)  goto error;
   // four word in a line
