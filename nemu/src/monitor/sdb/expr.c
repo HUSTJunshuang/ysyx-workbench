4ae764d261bc81464f6480094b3435f9e386937e
tracer-ysyx <tracer@ysyx.org>
2024-08-16 17:20:19 +0800

    > compile NEMU ysyx_? 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:20:19 up  8:59,  0 users,  load average: 0.16, 0.10, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -165,10 +165,11 @@ word_t eval(int p, int q) {
         }
       }
     }
-    Log("Main op = %d(%c)", op, tokens[op].type);
+    
     // calculate each
     word_t val1 = eval(p, op - 1);
     word_t val2 = eval(op + 1, q);
+    Log("Main op = %d(%c), L = %ld, R = %ld", op, tokens[op].type, val1, val2);
     switch (tokens[op].type) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
