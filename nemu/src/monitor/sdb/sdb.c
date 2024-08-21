1c4689a749a32842a79ed4973761bb92bd863cf0
tracer-ysyx <tracer@ysyx.org>
2024-08-21 14:49:29 +0800

    > compile NEMU ysyx_? 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:49:29 up  5:45,  0 users,  load average: 0.23, 0.15, 0.11

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -240,7 +240,6 @@ void sdb_mainloop() {
      * which may need further parsing
      */
     char *args = cmd + strlen(cmd) + 1;
-    printf("args in sdb_mainloop = %s\n", args);
     if (args >= str_end) {
       args = NULL;
     }
