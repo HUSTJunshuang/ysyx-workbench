1852a5239cc7535da607c1bc3a51b89bf077bc5c
tracer-ysyx <tracer@ysyx.org>
2024-09-17 23:10:35 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:10:35 up  2:07,  0 users,  load average: 0.27, 0.14, 0.14

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,12 +9,68 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
+// itoa function, for temporary use
+void reverse(char *s, int len) {
+  for (int i = 0; i < len / 2 - 1; i++) {
+    char tmp = s[len - i - 1];
+    s[len - i - 1] = s[i];
+    s[i] = tmp;
+  }
+}
+int itoa(int num, char *s) {
+  int len = 0;
+  while (num) {
+    s[len++] = '0' + (num % 10);
+    num /= 10;
+  }
+  s[len] = '\0';
+  reverse(s, len);
+  return len;
+}
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  size_t fmt_ptr = 0;
+  int d;
+  char *s;
+  int size = 0;
+
+  // clear out
+  *out = '\0';
+  while (fmt[fmt_ptr]) {
+    if (fmt[fmt_ptr++] == '%') {
+      switch (fmt[fmt_ptr++]) {
+      case '%':
+        out[size++] = '%';
+        break;
+      case 's':
+        s = va_arg(ap, char *);
+        size += strlen(s);
+        strcat(out, s);
+        break;
+      case 'd':
+        d = va_arg(ap, int);
+        // convert int to string
+        char dstr[16];
+        size += itoa(d, dstr);
+        strcat(out, dstr);
+        break;
+      }
+    }
+    else {
+      out[size++] = fmt[fmt_ptr++];
+    }
+    out[size] = '\0';
+  }
+  return size;
+  // panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int size = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return size;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
