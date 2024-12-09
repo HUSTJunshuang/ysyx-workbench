9a0b3f9a3b67895287ff0664c4a3933234d101c9
tracer-ysyx <tracer@ysyx.org>
2024-12-09 21:06:10 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:06:10 up  6:40,  0 users,  load average: 0.30, 0.26, 0.21

--- nemu/src/monitor/sdb/iringbuf.c
+++ nemu/src/monitor/sdb/iringbuf.c
@@ -9,6 +9,7 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 
 static iRB iringbuf = {iRB_CAP};
 char str_buf[128];
+const int ilen = 4;
 
 void init_iRB() {
     iringbuf.size = 0;
@@ -32,23 +33,38 @@ void push_iRB(vaddr_t pc, MUXDEF(CONFIG_ISA_x86, uint64_t, uint32_t) inst) {
 
 void print_iRB(vaddr_t pc) {
     int rptr = (iringbuf.wptr + iringbuf.capacity - iringbuf.size) % iringbuf.capacity;
-    printf("iringbuf.size = %d, wptr = %d, rptr = %d\n", iringbuf.size, iringbuf.wptr, rptr);
+    uint8_t *inst;
     // instructions executed
     for (int i = 0; i < iringbuf.size; ++i) {
-        disassemble(str_buf, sizeof(str_buf), iringbuf.pc_buf[rptr], (uint8_t *)(iringbuf.inst_buf + rptr), 4);
-        printf("%6s" FMT_WORD ": %s.\n", "", iringbuf.pc_buf[rptr], str_buf);
+        inst = (uint8_t *)&iringbuf.inst_buf[rptr];
+        disassemble(str_buf, sizeof(str_buf), iringbuf.pc_buf[rptr], inst, ilen);
+        printf("%6s" FMT_WORD ": %32s", "", iringbuf.pc_buf[rptr], str_buf);
+        for (int j = ilen - 1; j >= 0; --j) {
+            printf("%02x ", inst[j]);
+        }
+        printf("\n");
         rptr = (rptr + 1) % iringbuf.capacity;
     }
     // error instruction
-    MUXDEF(CONFIG_ISA_x86, uint64_t, uint32_t) inst = paddr_read(pc, 4);
-    disassemble(str_buf, sizeof(str_buf), pc, (uint8_t *)&inst, 4);
-    printf("  --> " FMT_WORD ": %s\n", pc, str_buf);
+    MUXDEF(CONFIG_ISA_x86, uint64_t, uint32_t) inst_val = paddr_read(pc, ilen);
+    inst = (uint8_t *)&inst_val;
+    disassemble(str_buf, sizeof(str_buf), pc, inst, ilen);
+    printf("  --> " FMT_WORD ": %32s", pc, str_buf);
+    for (int i = ilen - 1; i >= 0; --i) {
+        printf("%02x ", inst[i]);
+    }
+    printf("\n");
     // instructions behind
     for (int i = 0; i < TAIL_LEN; ++i) {
-        pc += 4;
-        inst = paddr_read(pc, 4);
-        disassemble(str_buf, sizeof(str_buf), pc, (uint8_t *)&inst, 4);
-        printf("%6s" FMT_WORD ": %s\n", "", pc, str_buf);
+        pc += ilen;
+        inst_val = paddr_read(pc, ilen);
+        inst = (uint8_t *)&inst_val;
+        disassemble(str_buf, sizeof(str_buf), pc, inst, ilen);
+        printf("%6s" FMT_WORD ": %32s", "", pc, str_buf);
+        for (int j = ilen - 1; j >= 0; --j) {
+            printf("%02x ", inst[j]);
+        }
+        printf("\n");
     }
 }
 
