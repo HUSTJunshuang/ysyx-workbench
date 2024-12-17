40070f508b86a673aff86e0388aa763714381d23
tracer-ysyx <tracer@ysyx.org>
2024-12-18 01:10:47 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:10:47 up 11:19,  0 users,  load average: 0.30, 0.23, 0.18

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -35,8 +35,8 @@ void init_icb(const char *elf_file) {
     // check section header table
     Assert(Ehdr.e_shoff, "'%s' has no section header table", elf_file);
     Assert(Ehdr.e_shstrndx != SHN_UNDEF, "'%s' has no shstrtab", elf_file);
-    // MUXDEF(CONFIG_ISA64, Elf64_Shdr, Elf32_Shdr) shstrtab_shdr;
-    // fseek(icb.elf_fp, Ehdr.e_shoff + sizeof(shstrtab_shdr) * Ehdr.e_shstrndx, SEEK_SET);
+    MUXDEF(CONFIG_ISA64, Elf64_Shdr, Elf32_Shdr) shstrtab_shdr;
+    fseek(icb.elf_fp, Ehdr.e_shoff + sizeof(shstrtab_shdr) * Ehdr.e_shstrndx, SEEK_SET);
     // // TODO index较大会放在另一个位置
     // Assert(fread(&shstrtab_shdr, sizeof(shstrtab_shdr), 1, icb.elf_fp) == 1, "Read .shstrtab failed");
     // uint64_t section_num = Ehdr.e_shnum;
