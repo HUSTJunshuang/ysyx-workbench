1dee3513ae57708eccd6b17747813be075e1ba97
tracer-ysyx <tracer@ysyx.org>
2024-12-18 01:39:13 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:39:13 up 11:47,  0 users,  load average: 0.23, 0.19, 0.18

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -53,8 +53,12 @@ void init_icb(const char *elf_file) {
         Assert(fread(&shdr, sizeof(shdr), 1, icb.elf_fp) == 1, "Read Elf%d_Shdr[%d] failed", XLEN, i);
         
         fseek(icb.elf_fp, shstrtab_shdr.sh_offset + shdr.sh_name, SEEK_SET);
+        char test[64];
+        int cnt = fscanf(icb.elf_fp, "%s%s", sec_name, test);
+        printf("cnt = %d\n", cnt);
         // Assert(fscanf(icb.elf_fp, "%s", sec_name), "Read Section Name[%d] failed", i);
         printf("Sec[%d] = %s, len = %ld\n", i, sec_name, strlen(sec_name));
+        printf("test = %s\n", test);
         // if (strcmp(sec_name, ".symtab") == 0) {
         //     symtab_shdr = shdr;
         // }
