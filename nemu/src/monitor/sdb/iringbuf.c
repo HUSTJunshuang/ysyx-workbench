f69e96c1fce8b5968ae9370507e6540d96008a54
tracer-ysyx <tracer@ysyx.org>
2024-12-18 23:04:31 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:04:31 up 44 min,  0 users,  load average: 0.23, 0.27, 0.26

--- nemu/src/monitor/sdb/iringbuf.c
+++ nemu/src/monitor/sdb/iringbuf.c
@@ -58,8 +58,8 @@ void print_iRB(vaddr_t pc) {
     MUXDEF(CONFIG_ISA_x86, uint64_t, uint32_t) inst_val = host_read(guest_to_host(pc), ilen);
     inst = (uint8_t *)&inst_val;
     printf("  --> " FMT_WORD ":", pc);
-    display_inst(inst, ilen);printf("breakpoint\n");
-    disassemble(str_buf, sizeof(str_buf), pc, inst, ilen);
+    display_inst(inst, ilen);
+    disassemble(str_buf, sizeof(str_buf), pc, inst, ilen);printf("breakpoint\n");
     printf("\t%s\n", str_buf);
     // instructions behind
     for (int i = 0; i < TAIL_LEN; ++i) {
