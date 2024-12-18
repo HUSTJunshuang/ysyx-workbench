b935152209ce741f8603cc7d4cd2d0ffeb0e393d
tracer-ysyx <tracer@ysyx.org>
2024-12-18 23:08:53 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:08:53 up 48 min,  0 users,  load average: 0.14, 0.18, 0.22

--- nemu/src/monitor/sdb/iringbuf.c
+++ nemu/src/monitor/sdb/iringbuf.c
@@ -42,7 +42,7 @@ void print_iRB(vaddr_t pc) {
     int rptr = (iringbuf.wptr + iringbuf.capacity - iringbuf.size) % iringbuf.capacity;
     uint8_t *inst;
     // check started or not
-    // if (iringbuf.size == 0) return ;
+    if (iringbuf.size == 0) return ;
     printf(ANSI_FMT("Encountered an error while executing the following snippets:", ANSI_FG_MAGENTA) "\n");
     if (iringbuf.size == iringbuf.capacity) printf("%6s......\n", "");
     // instructions executed
@@ -58,7 +58,7 @@ void print_iRB(vaddr_t pc) {
     MUXDEF(CONFIG_ISA_x86, uint64_t, uint32_t) inst_val = host_read(guest_to_host(pc), ilen);
     inst = (uint8_t *)&inst_val;
     printf("  --> " FMT_WORD ":", pc);
-    display_inst(inst, ilen);printf("breakpoint\n");
+    display_inst(inst, ilen);
     disassemble(str_buf, sizeof(str_buf), pc, inst, ilen);
     printf("\t%s\n", str_buf);
     // instructions behind
