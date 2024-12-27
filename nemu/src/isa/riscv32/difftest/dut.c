dfe3c243920c7b4080b8fa8ca1c72fe17713f5b3
tracer-ysyx <tracer@ysyx.org>
2024-12-27 16:23:28 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:23:28 up  1:00,  0 users,  load average: 0.42, 0.37, 0.29

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,6 +19,7 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   printf("breakpoint\n");
+  printf("pc = " FMT_WORD "\n", pc);
   if (pc != cpu.pc) return false;
   for (int idx = 0; idx < MUXDEF(CONFIG_RVE, 16, 32); idx++) {
     if (ref_r->gpr[idx] != gpr(idx))  return false;
