d48c17751c8b8e1461ff6cb3bd4cc8a310475945
tracer-ysyx <tracer@ysyx.org>
2025-01-11 22:03:59 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:03:59 up  6:01,  0 users,  load average: 0.19, 0.34, 0.32

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -40,7 +40,7 @@ void *malloc(size_t size) {
 // #endif
   if (hbrk == NULL) {
     hbrk = (void *)ROUNDUP(heap.start, 8);
-    printf("heap.start = %d\n", heap.start);
+    printf("heap.start = %ld\n", heap.start);
     halt(0);
   }
   if (size == 0)  return NULL;
