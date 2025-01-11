b0ccc63025a200a66e2d1aa90f2ba7c8456453f8
tracer-ysyx <tracer@ysyx.org>
2025-01-11 21:45:19 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:45:19 up  5:43,  0 users,  load average: 0.73, 0.36, 0.25

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,6 +29,8 @@ int atoi(const char* nptr) {
   return x;
 }
 
+static char* hbrk = NULL;
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
@@ -36,7 +38,13 @@ void *malloc(size_t size) {
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   panic("Not implemented");
 #endif
-  return NULL;
+  if (hbrk == NULL) hbrk = (void *)ROUNDUP(heap.start, 8);
+  if (size == 0)  return NULL;
+  size = (size_t)ROUNDUP(size, 8);
+  char *old = hbrk;
+  hbrk += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
+  return old;
 }
 
 void free(void *ptr) {
