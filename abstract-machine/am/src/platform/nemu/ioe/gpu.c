7f88b69485180e7fb972765ef024293d17bda07f
tracer-ysyx <tracer@ysyx.org>
2025-01-12 16:47:40 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:47:40 up  2:55,  0 users,  load average: 0.72, 0.42, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -21,22 +21,22 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  // if (ctl->pixels != NULL) {
-  //   int hw = inl(VGACTL_ADDR);
-  //   int w = (hw & 0xffff0000) >> 16;
-  //   // int h = hw & 0x0000ffff;
-  //   uint32_t *fb = (uint32_t *)FB_ADDR;
-  //   for (int dy = 0; dy < ctl->h; ++dy) {
-  //     for (int dx = 0; dx < ctl->w; ++dx) {
-  //       fb[(ctl->x + dx) + (ctl->y + dy) * w] = ((uint32_t *)ctl->pixels)[dx + dy * ctl->w];
-  //     }
-  //   }
-  // }
-  int hw = inl(VGACTL_ADDR);
-  int w = (hw & 0xffff0000) >> 16;
-  int h = hw & 0x0000ffff;
-  uint32_t *fb = (uint32_t *)FB_ADDR;
-  for (int i = 0; i < w * h; ++i) fb[i] = i;
+  if (ctl->pixels != NULL) {
+    int hw = inl(VGACTL_ADDR);
+    int w = (hw & 0xffff0000) >> 16;
+    // int h = hw & 0x0000ffff;
+    uint32_t *fb = (uint32_t *)FB_ADDR;
+    for (int dy = 0; dy < ctl->h; ++dy) {
+      for (int dx = 0; dx < ctl->w; ++dx) {
+        fb[(ctl->x + dx) + (ctl->y + dy) * w] = ((uint32_t *)ctl->pixels)[dx + dy * ctl->w];
+      }
+    }
+  }
+  // int hw = inl(VGACTL_ADDR);
+  // int w = (hw & 0xffff0000) >> 16;
+  // int h = hw & 0x0000ffff;
+  // uint32_t *fb = (uint32_t *)FB_ADDR;
+  // for (int i = 0; i < w * h; ++i) fb[i] = i;
   if (ctl->sync) {putstr("invoked __am_gpu_fbdraw and sync\n");
     outl(SYNC_ADDR, 1);
   }
