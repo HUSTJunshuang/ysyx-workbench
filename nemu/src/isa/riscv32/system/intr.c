44d41a01ad18fc77a199b74580d37929bdf62159
tracer-ysyx <tracer@ysyx.org>
2025-02-22 17:14:49 +0800

    > compile NEMU ysyx_24090011 蒋俊爽 Linux DESKTOP-E9OIDU0 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:14:49 up  4:28,  0 users,  load average: 0.34, 0.16, 0.12

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -15,12 +15,27 @@
 
 #include <isa.h>
 
+static const char *intr_desc[256] = {
+  [0]   = "Reserved",
+};
+
+static const char *excp_desc[256] = {
+  [8]   = "Environment call from U-mode or VU-mode",
+  [9]   = "Environment call from HS-mode",
+  [10]  = "Environment call from VS-mode",
+  [11]  = "Environment call from S-mode",
+};
+
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
   cpu.csr[mepc] = epc;
   cpu.csr[mcause] = NO;
+  word_t interrupt = NO >> MUXDEF(CONFIG_RV64, 63, 31);
+  word_t index = (NO << 1) >> 1;
+  Log("Raised %s(trap code " FMT_DEC "): %s at pc = " FMT_WORD, interrupt ? "interrupt" : "exception", NO,
+      interrupt ? intr_desc[index] : excp_desc[index], epc);
 
   return cpu.csr[mtvec];
 }
